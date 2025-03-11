a997c60fa55f35c9b04eb43bbd8a9936c705c868
Fabrice MUKUNZI <mukunzifabrice77@gmail.com>
2025-03-11 23:54:30 +0200

    Frontend-[chore]: boilerplate setup with shadcn installed

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button className="h-11">Frontend setup is done 😜</Button>
+    </div>
+  );
+}
