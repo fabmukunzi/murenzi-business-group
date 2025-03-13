53353c77cdd7e36329119a7d32a2c48cf7a4f9df
Fabrice MUKUNZI <mukunzifabrice77@gmail.com>
2025-03-13 07:13:35 +0200

    Frontend-[feat]: add footer and header

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-[70vh]">
       <Button className="h-11">Frontend setup is done 😜</Button>
     </div>
   );
---
+++ frontend/lib/routes.ts
@@ -0,0 +1,27 @@
+export const routes = {
+    home: {
+      label: 'Home',
+      path: '/',
+      name: 'home',
+    },
+    rentals: {
+      label: 'Rentals',
+      path: '/rentals',
+      name: 'rentals',
+    },
+    booth: {
+      label: 'Booth',
+      path: '/booth',
+      name: 'booth',
+    },
+    saunaMassage: {
+      label: 'Sauna & Massage',
+      path: '/sauna-and-massage',
+      name: 'sauna-and-massage',
+    },
+    contact: {
+      label: 'Contact Us',
+      path: '#contact',
+      name: 'contact',
+    },
+  };
