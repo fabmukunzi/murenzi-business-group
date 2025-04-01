422342eb4f5d4d6b6e3126830743f09f19494d4b
Fabrice MUKUNZI <mukunzifabrice77@gmail.com>
2025-04-01 21:57:52 +0200

    [fix]: rubik font error

--- frontend/app/(auth)/layout.tsx
+++ frontend/app/(auth)/layout.tsx
@@ -3,11 +3,12 @@ import '@/app/globals.css';
 import { SessionProvider } from "next-auth/react";
 import { Rubik } from 'next/font/google';
 
+const rubik = Rubik({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const rubik = Rubik({
-    variable: '--font-geist-sans',
-    subsets: ['latin'],
-  });
+
   return (
     <SessionProvider>
       <html lang="en">
